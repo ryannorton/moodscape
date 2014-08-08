6f9f053b2b5bb39ed8a2981662701a9a71da9320
ryannorton <nort.ryan@gmail.com>
2014-08-07 21:04:09 -0600

    updated css and map files

--- static/js/main.js
+++ static/js/main.js
@@ -1,26 +1,47 @@
 // fetches tweets from database and injects into html
-function write_tweets() {$.getJSON('/tweets',
+function get_tweets(start, n) {$.getJSON('/tweets/' + start + '/' + n,
 		function (tweets) {
 			if (tweets) {
+				// write tweets
 				var html = '<ul class="list-group">';
-				$.each(tweets, function(i, tweet) {
-					html += '<li class="list-group-item"> <strong>' + tweet.fields.user + '</strong>: '
-					html += tweet.fields.text + ' (' + tweet.fields.lat + ',' + tweet.fields.lon + ')</li>'
+				$.each(tweets.slice(0,5), function(i, tweet) {
+					html += '<li class="list-group-item"> <strong>' + tweet.fields.user + '</strong>: ('
+					html += tweet.fields.sentiment + ') ' + tweet.fields.text + ' (' + tweet.fields.lat + ',' + tweet.fields.lon + ')</li>'
 				});
 				html += '</ul>'
-				$('#tweets .container-fluid #tweet').html(html);
+				$('#tweets #tweet').html(html);
+
+				// map tweets
+				$.each(tweets, function(i, tweet) {
+					console.log(tweet);
+					var lat = tweet.fields.lat;
+					var lon = tweet.fields.lon;
+					map.plot(lon, lat, tweet.fields.user + ': ' + tweet.fields.text, tweet.fields.url);
+					var coords = LatLonToHeat(lon, lat);
+					heatmap.addData({
+						x: coords[0],
+						y: coords[1],
+						value: tweet.fields.sentiment,
+					});
+				});
 			}
 		}
 	);
 }
 
-// fetches tweets from database and injects into html
+// fetches tweets from database and plots on map and heatmap
 function plot_tweets() {$.getJSON('/tweets',
 		function (tweets) {
 			$.each(tweets, function(i, tweet) {
 				var lat = tweet.fields.lat;
 				var lon = tweet.fields.lon;
-				map.plot(lon, lat);
+				map.plot(lon, lat, tweet.fields.user + ': ' + tweet.fields.text, tweet.fields.url);
+				var coords = LatLonToHeat(lon, lat);
+				heatmap.addData({
+					x: coords[0],
+					y: coords[1],
+					value: tweet.fields.sentiment,
+				});
 			});
 		}
 	);
@@ -30,7 +51,20 @@ function plot_tweets() {$.getJSON('/tweets',
 function tweetstream(rate) {
 	setInterval(function () {
 		plot_tweets();
+		write_tweets();
 	}, rate);
+}
+
+function LatLonToHeat(lat, lon) {
+    var xOffset = -17;
+    var yOffset = -22;
+    var scaleX = 10.05;
+    var scaleY = 6.26;
+    var i = 0.842;
+
+    var x = 50.0 + 124.03149777329222 * ((1.9694462586094064-(lat* Math.PI / 180)) * Math.sin(0.6010514667026994 * (lon + 96) * Math.PI / 180));
+    var y = 50.0 + 1.6155950752393982 * 124.03149777329222 * 0.02613325650382181 - 1.6155950752393982  * 124.03149777329222 * (1.3236744353715044  - (1.9694462586094064-(lat* Math.PI / 180)) * Math.cos(0.6010514667026994 * (lon + 96) * Math.PI / 180));
+  	return([((x * scaleX + xOffset) * i), ((y * scaleY + yOffset) * i)]);
 }
 
 // creates svg map
@@ -39,6 +73,19 @@ var container = 'map';
 var paper = new ScaleRaphael(container, 950, 650);
 var map = paper.USMap();
 paper.scaleAll(MapWidth/map.width);
-tweetstream();
 
-//map.darkenState('PA', 1.07);
+// creates heatmap overlay
+var heatmap = h337.create({container: document.getElementById('map'), radius: 40});
+heatmap.setDataMax(1.0);
+heatmap.setDataMin(-1.0);
+
+function doSetTimeout(i) {
+	setTimeout(function () {
+		get_tweets(i*500, 500);
+	}, i*2000);
+}
+
+for (var i = 0; i<90; i++) {
+	doSetTimeout(i);
+}
+
